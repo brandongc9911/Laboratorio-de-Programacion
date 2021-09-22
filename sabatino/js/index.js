df964de0c2045f33654f8a4f76d91b96df918889
brandongc9911 <brandongc9911@gmail.com>
2021-09-21 21:54:02 -0500

    comentarios

--- sabatino/js/index.js
+++ sabatino/js/index.js
@@ -1,51 +1,61 @@
 // 'use strict';
-// var edad1 = 18
-// const edad2 = 18
-
-// edad1 = 20
-// // edad2 = 20
-
-// console.log(edad1)
-// console.log(edad2)
 
 
-// let edad3 = 18
+// Ejemplos de var,const y let
+var edad1 = 18
+const edad2 = 18
 
-// edad3 = 20
+edad1 = 20
+edad2 = 20
 
-// console.log(edad3)
-
-
-// var palabra = 'Programacion'
-
-// console.log(palabra[0])
-
-// console.log(palabra.slice(0,3))
+console.log(edad1)
+console.log(edad2)
 
 
-// var lista = [1,2,3,4,5]
+let edad3 = 18
 
-// lista[0] = 0
+edad3 = 20
 
-// console.table(lista)
-
-
+console.log(edad3)
 
 
+var palabra = 'Programacion'
+
+// Acceder individualmente a una letra del string
+console.log(palabra[0])
+
+// Acceder por una porcion del string
+console.log(palabra.slice(0,3))
+
+
+// Lista
+var lista = [1,2,3,4,5]
+
+lista[0] = 0
+
+// Mejora visual en terminal con console.log - Crea una tabla
+console.table(lista)
+
+
+
+// concatenacion
 var palabra = 'Hola'
 var palabra2 = 'Mundo'
-
 
 console.log(`${palabra}` +  ' ' +  `${palabra2}`)
 
 
 var numero = 3
 
-// numero++
-// ++numero
-// console.log(numero)
+
+// iterar un numero en JS
+numero++
+++numero
+console.log(numero)
 
 
+
+// Tipos de datos 
 var numero = 5
 var numero2 = '5'
 
@@ -53,26 +63,25 @@ console.log(typeof(numero))
 console.log(typeof(numero2))
 
 
-// console.log(numero * numero2)
+console.log(numero * numero2)
 
 
-// var resultado = numero * numero2
+var resultado = numero * numero2
 
-// console.log(typeof(resultado))
-
-
-
-// console.log(numero === numero2)
+console.log(typeof(resultado))
 
 
-// console.log(parseInt(numero2))
-// console.log(String(numero))
 
+console.log(numero === numero2)
+
+
+console.log(parseInt(numero2))
+console.log(String(numero))
+
+
+// Objetos 
 
 // object literal
-
-
-
 const producto = {
     nombre: 'Computadora',
     precio: 20000,
@@ -86,13 +95,21 @@ const producto2 = {
     memororia: '1tb',
     so: '11'
 }
-// Object.freeze(producto)
+
+// Cuando trabajamos con objetos si el objeto se declara con const aun se puede modificar, hay metodos que nos ayudar a obligarlo a no cambiar
+
+
+// Object.freeze(producto) -- No permite cambiar nada del objeto
 // console.log(Object.isFrozen(producto))
 
-// object.seal(product)
+// object.seal(product) --permite unicamente modificar los  datos existentes del objeto
+
+
 // console.log(producto.memororia = '20gb' )
 
 
+
+// Object destructuring
 // const {nombre} = producto
 // console.log(nombre)
 
--- sabatino/python/main.py
+++ sabatino/python/main.py
@@ -1,45 +1,57 @@
-edad =  18
 
+# Variables en Python
+edad =  18
 print(edad)
 
-
+# Cuando trabajamos con string, o arrays(listas y tambien las tuplas) python les da un indice a cada elemento. Algo en tener en cuenta es que la manera en que enumera
+# es apartir del 0. Recuerda los idices python los maneja como n-1, esto quiere decir que el elemento que tengamos le resta 1.
 # 0,1,2,3,4,5
 # -6,-5,-4,-3,-2,-1
 
 
-# palabra = 'Programacion'
+
+palabra = 'Programacion'
+
+# Accediendo a un elemento individualmente
+print(palabra[3])
+
+#Accediendo a una porcion (slice)
+print(palabra[0:3])
+
+#Reaccinado valor de un indice
+palabra[2] = 'O'
+print(palabra)
 
 
-# palabra[2] = 'O'
+# lista
+lista = [1,2,3,4,5] # mutable
 
-# print(palabra)
-# print(palabra[3])
+# tupla
+lista2 = (1,2,3,4,5) # no mutable
 
-# print(palabra[::2])
+print(type(lista))
+print(type(lista2))
 
-lista = [1,2,3,4,5]
-# lista2 = (1,2,3,4,5)
-
-print(lista[0:3])
-
-# print(type(lista))
-# print(type(lista2))
+# Tanto las listas y las tuplas podemos acceder a ellas indidivualmente y por slices 
 
 
 
+
+# concatenacion
 palabra = 'Hola'
 palabra2 = 'Mundo'
-
 print(palabra + ' ' + palabra2)
 
 
-numero = 1
 
+# Manera de iterar un numero en python
+numero = 1
 numero += 1
 
 print(numero)
 
 
+# Covertir tipo de datos en Python
 numero = 5
 numero2 = "5"
 
